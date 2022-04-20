d2a0933b704b536f06cec08331ad586818582843
Your Name <irving.jem@gmail.com>
2022-04-20 12:24:12 -0700

    fixing up route files to make server file better

---
+++ routes/html_routes/index.js
@@ -0,0 +1,3 @@
+// Node modules
+const express = require('express');
+const router = express.Router();
--- server.js
+++ server.js
@@ -8,56 +8,16 @@ const routeHtml = require('./routes/html_routes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Setup data parsing
+// Setup middleware 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/api', routeApi);
+app.use('/', routeHtml);
 
-//Route for notes file
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-// route for main page
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-// Route for .json reading
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
-});
-
-
-// save note adding to db.json
-app.post("/api/notes", (req, res) => {
-    let newNote = req.body;
-    let listNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let noteLength = (noteList.length).toString();
-
-    // adding an id to newNote
-    newNote.id = noteLength;
-    // push note to list in the db.json
-    listNote.push(newNote);
-
-    // write the .json using stringify to the db.json
-    fs.writeFileSync("./db/db.json", JSON.stringify(listNote));
-    res.json(listNote)
-})
-
-// Delete note based on id that is assigned to it
-app.delete("/api/notes/:id", (req, res) => {
-    let listNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let noteId = (req.params.id).toString();
-
-    // filter through ids so there are not duplicates
-    listNote = listNote.filter(selected => {
-        return selected.id != noteId;
-    })
-
-    // then update the .json data so the note is correctly displayed
-    fs.writeFileSync("./db/db.json", JSON.stringify(listNote));
-    res.json(listNote);
+// err response for bad requests
+app.use((req, res) => {
+    res.status(404).end();
 });
 
 
