950607a1bc3b4d50642cb508f86cc2da76158918
Your Name <irving.jem@gmail.com>
2022-04-20 12:29:10 -0700

    added saving note and deleting note to api route

--- routes/api_routes/index.js
+++ routes/api_routes/index.js
@@ -1,3 +1,39 @@
 // Node modules
 const express = require('express');
 const router = express.Router();
+
+
+// save note adding to db.json
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    let listNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteLength = (noteList.length).toString();
+
+    // 
+    newNote.id = noteLength;
+    // push note to list in the db.json
+    listNote.push(newNote);
+
+    // write the .json using stringify to the db.json
+    fs.writeFileSync("./db/db.json", JSON.stringify(listNote));
+    res.json(listNote)
+})
+
+
+
+// Delete note based on id that is assigned to it
+app.delete("/api/notes/:id", (req, res) => {
+    let listNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteId = (req.params.id).toString();
+
+    // filter through ids so there are not duplicates
+    listNote = listNote.filter(selected => {
+        return selected.id != noteId;
+    })
+
+    // then update the .json data so the note is correctly displayed
+    fs.writeFileSync("./db/db.json", JSON.stringify(listNote));
+    res.json(listNote);
+})
+
+module.exports = router;
--- routes/html_routes/index.js
+++ routes/html_routes/index.js
@@ -1,3 +1,15 @@
 // Node modules
 const express = require('express');
 const router = express.Router();
+
+// finds notes file to send notes.html file
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+// route for index.html
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+module.exports = router;
